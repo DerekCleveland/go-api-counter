543cbcd280669fd49c66daeb2c4eac6158156d8b
Derek Cleveland <clevelanddtc@gmail.com>
2020-10-06 00:49:54 -0400

    updated function name

--- cmd/GoApiCounter/main.go
+++ cmd/GoApiCounter/main.go
@@ -8,14 +8,14 @@ import (
 
 var counter int
 
-func handleHello(w http.ResponseWriter, r *http.Request) {
+func handleCounter(w http.ResponseWriter, r *http.Request) {
 	counter++
 	fmt.Fprintf(w, "COUNTER = %d", counter)
 }
 
 func main() {
 	counter = 0
-	http.HandleFunc("/", handleHello)
+	http.HandleFunc("/", handleCounter)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
